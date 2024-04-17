d1dd67c2d1a5b432ac749f5422554109d52c6f96
MIN0707 <alsrb4171@gmail.com>
2024-04-17 20:59:42 +0900

    ♻️ Rename routers folder

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express') // express 모듈을 불러온다.
 const app = express() // express를 app 변수에 담는다.
 app.listen(3000, () => console.log('서버 시작')) // 서버 시작 코드
 
-app.use('/', require('./routers/root'))
+app.use('/', require('./routes/root'))
