2d06c8536ab145f0ebd45b73373041d29ed1444d
seo20201010 <157564032+seo20201010@users.noreply.github.com>
2024-04-17 20:49:53 +0900

    testing1

---
+++ learn.js
@@ -0,0 +1,49 @@
+// 주석 다는법
+// 슬래시 두번 = 파이선 #
+
+// 변수 생성법
+
+/*
+
+
+
+a = 1
+const a = 1
+let a = 1
+
+const = 변할 수 없음
+let = 변할 수 있음
+
+python import
+JS require()
+
+
+
+respone 이 res라고 했을때
+
+res안의 send라는 함수가 전달해주는 함수다
+res.send()
+
+example
+
+res.send('ㅁㄴㅇㄹ')
+
+
+
+
+
+
+*/
+
+/*
+
+미션1#
+
+http://localhost:3000/users/getUser
+
+{
+  name: "홍길동",
+  age: 25
+}
+
+*/
--- routers/root.js
+++ routers/root.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('this is /')
+  const user = {
+    name: '홍길동',
+    age: 25,
+  }
+  res.send(user)
 })
 
 module.exports = router
